f57c373bdeef5ddb22f8d7b89d4c1ddf03c4ab80
paco <fraurerod@gmail.com>
2020-02-05 20:22:59 +0100

    update builder director

--- DesignPatternsTutorial/CreationalDesignPatterns/Builder/Wizard.cs
+++ DesignPatternsTutorial/CreationalDesignPatterns/Builder/Wizard.cs
@@ -2,6 +2,23 @@ namespace DesignPatternsTutorial.CreationalDesignPatterns.Builder
 {
     public class Wizard
     {
-        
+        private readonly IPotionCauldron _potionCauldron;
+
+        public Wizard(IPotionCauldron potionCauldron)
+        {
+            _potionCauldron = potionCauldron;
+        }
+
+        public Potion GetPotion()
+        {
+            return _potionCauldron.GetPotion();
+        }
+
+        public void MakePotion()
+        {
+            _potionCauldron.SetPotionBase();
+            _potionCauldron.SetMainIngredient();
+            _potionCauldron.SetEnhancers();
+        }
     }
 }
